bcbd452a87b210218ae4a787c16205d174169fe0
Nícolas Teixeira <nicolasteixeira@localhost.localdomain>
2019-11-09 20:12:49 -0300

    Pequenas mudanças no shell

--- 1 Periodo/Administração de Sistemas/backup.sh
+++ 1 Periodo/Administração de Sistemas/backup.sh
@@ -1,8 +1,29 @@
-origem=./Backup	
-destino=./BackupRSYNC	
-logfile=$origem/logsync.txt	
+origemUm=~/Documentos	
+origemDois=~/Imagens	
+destinoUm=./BackupDocumentos	
+destinoDois=./BackupImagens
+logfile=$destino/logsync.txt	
 
-echo $origem
-echo $destino
+echo "***********************************************************************"
+echo "Bem-vindo ao script de backup automático"
+echo "As pastas automaticamente sincronizadas serão: Imagens e Documentos"
+echo "***********************************************************************"
 
-rsync -avzh -progress --delete --exclude='.DS_Store' --delete-excluded --log-file=$logfile $origem/ $destino/
+for DEV in /sys/block/sd*
+do
+	if readlink $DEV/device | grep -q usb
+	then
+		DEV=`basename $DEV`
+		echo "$DEV is a USB device, info:"
+		udevinfo --query=all --name $DEV
+		if [ -d /sys/block/${DEV}/${DEV}1 ]
+		then
+			echo "Has partitions " /sys/block/$DEV/$DEV[0-9]*
+		else
+			echo "Has no partitions"
+		fi
+		echo
+	fi
+done
+
+# rsync -avzh -progress --delete --exclude='.DS_Store' --delete-excluded --log-file=$logfile $origemUm/ $destinoUm/
